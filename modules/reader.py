6ca969eb2a9415639972d99ac31be16364784d44
Max Barnes <maxbarnes.dev@gmail.com>
2024-12-02 18:07:18 -0500

    file reader should just be txt file, no reason to do csv in this case

--- modules/reader.py
+++ modules/reader.py
@@ -1,6 +1,13 @@
 # read the csv and create a list with all answers and hints
+import csv
 
-file = ''
+# file = open("demo.csv","r")
+
+with open('demo.csv') as file:
+    test = csv.reader(file)
+
+for row in test:
+    print(row)
+
 
 # initialize dictionary for leaderboard
-
